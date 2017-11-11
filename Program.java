339d39c939d3f9f9a7dd76006f0d24455ef6ecc8
Rendra96 <33146490+Rendra96@users.noreply.github.com>
2017-11-11 15:34:18 +0700

    Update Program

--- Program.java
+++ Program.java
@@ -1,35 +1,43 @@
 
 import java.util.List;
 
-class Program
-{
-    public static int TotalTime(int[] band, int n)
-    {
-        if (n < 3)
-        {
-            return band[n - 1];
-        }
-        else if (n == 3)
-        {
-            return band[0] + band[1] + band[2];
-        }
-        else
-        {
-            int temp1 = band[n - 1] + band[0] + band[n - 2] + band[0];
-            int temp2 = band[1] + band[0] + band[n - 1] + band[1];
+class Program {
 
-            if (temp1 < temp2)
-            {
+    public static int TotalTime(int[] band, int n) {
+        if (n < 3) {
+            int temp = band[n - 1];
+            System.out.println("yang terakhir " + temp);
+            return temp;
+        } else if (n == 3) {
+            int temp = band[0] + band[1] + band[2];
+            System.out.println("GO "+ band[2] + " and " + band[0]);
+            System.out.println("balik lagi " + band[0] );
+            System.out.println("GO "+ band[1] + " and "+band[0]);
+            return temp;
+        } else {
+            int temp1 = band[n - 1] + band[0] + band[n - 2] + band[0];
+            //int temp2 = band[1] + band[0] + band[n - 1] + band[1];
+
+            /*if (temp1 < temp2) {
+                System.out.println("GO " + band[n - 1] + " and " + band[0]);
+                System.out.println("balik lagi " + band[0]);
+                System.out.println("GO " + band[n - 2] + " and " + band[0]);
+                System.out.println("balik lagi " + band[0]);
                 return temp1 + TotalTime(band, n - 2);
-            }
-            else if (temp2 < temp1)
-            {
+            } else if (temp2 < temp1) {
+                System.out.println("GO " + band[1] + " and " + band[0]);
+                System.out.println("balik lagi " + band[0]);
+                System.out.println("GO " + band[n - 1] + " and " + band[1]);
+                System.out.println("balik lagi " + band[1]);
                 return temp2 + TotalTime(band, n - 2);
-            }
-            else
-            {
+            } else {
+                System.out.println("GO " + band[1] + " and " + band[0]);
+                System.out.println("balik lagi " + band[0]);
+                System.out.println("GO " + band[n - 1] + " and " + band[1]);
+                System.out.println("balik lagi " + band[1]);
                 return temp2 + TotalTime(band, n - 2);
-            }
+            }*/
+            return temp1 + TotalTime(band, n - 2);
         }
     }
 }
--- Tester.java
+++ Tester.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,12 +15,15 @@ public class Tester {
     public static void main(String[] args) {
         // change the no of people crossing the bridge
         // add or remove corresponding time to the list
-        int n = 4; 
-
-        int[] band = { 1, 2, 5, 10 };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Banyak orang yang ingin menyeberang : ");
+        int n = sc.nextInt();
         
-
-        //band.Sort();
+        System.out.println("Waktu menyeberang setiap orang :");
+        int[] band = new int[n];
+        for(int i = 0;i<band.length;i++) {
+            band[i] = sc.nextInt();
+        }
 
         System.out.println("The total time taken to cross the bridge is: " + Program.TotalTime(band, n));
     }
